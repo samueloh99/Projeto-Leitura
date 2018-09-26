a53e15fbbc34f5d0f32ac6f244ba2a2e0742e9ab
Vinicius <disanti2010@gmail.com>
2018-09-26 19:41:24 -0300

    feat: Ordenando por data ou timestamp na listagem de posts

--- src/components/ListPosts.js
+++ src/components/ListPosts.js
@@ -2,27 +2,41 @@ import React, { Component } from "react";
 import Post from "./Post";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 class ListPosts extends Component {
   state = {
     posts: [],
-    descending: true
+    descending: true,
+    sortField: "voteScore"
   };
 
   componentWillReceiveProps(nextProps) {
     const { posts } = nextProps || [];
-    this.setState({ posts: this.sortPosts(posts) });
+    this.setState({ posts }, () => {
+      this.sortPosts();
+    });
   }
 
-  sortPosts = posts => {
-    const { descending } = this.state;
+  changeSort = e => {
+    const sortField = e.target.value;
+    this.setState({ sortField }, () => {
+      this.sortPosts();
+    });
+  };
+
+  sortPosts = () => {
+    const { posts, sortField, descending } = this.state;
     if (posts && posts.length) {
       posts.sort((postA, postB) => {
-        if (descending) return postB.voteScore - postA.voteScore;
-        return postA.voteScore - postB.voteScore;
+        if (descending) return postB[sortField] - postA[sortField];
+        return postA[sortField] - postB[sortField];
       });
+      this.setState({ posts });
     }
-    return posts;
   };
 
   sortList = () => {
@@ -31,7 +45,7 @@ class ListPosts extends Component {
         descending: !this.state.descending
       },
       () => {
-        this.setState({ posts: this.sortPosts(this.state.posts) });
+        this.sortPosts();
       }
     );
   };
@@ -40,8 +54,24 @@ class ListPosts extends Component {
     const { posts, descending } = this.state;
     return (
       <div>
+        <form autoComplete="off">
+          <FormControl className="select-input">
+            <InputLabel htmlFor="tipo-order">Ordenar por:</InputLabel>
+            <Select
+              value={this.state.sortField}
+              onChange={this.changeSort}
+              inputProps={{
+                name: "order",
+                id: "tipo-order"
+              }}
+            >
+              <MenuItem value="voteScore">Pontuação</MenuItem>
+              <MenuItem value="timestamp">Data de criação</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
         <Button className="sort-posts" onClick={() => this.sortList()}>
-          Posts
+          Crescente / Decrescente
           <Icon>{descending ? "arrow_downward" : "arrow_upward"}</Icon>
         </Button>
         {posts &&
